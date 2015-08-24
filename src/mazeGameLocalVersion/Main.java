b9b71442dd0f9e5e278ad0bef5b0c5f92a782d2b
Benze Tang <tangbenze@gmail.com>
2015-08-24 14:42:01 +0800

    local main

--- src/mazeGameLocalVersion/Main.java
+++ src/mazeGameLocalVersion/Main.java
@@ -3,7 +3,7 @@ package mazeGameLocalVersion;
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.println("This is a maze game local version");
+		System.out.println("This is a mazegame.maze game local version");
 	}
 
 }
--- src/mazegame/maze/MazePlayer.java
+++ src/mazegame/maze/MazePlayer.java
@@ -7,6 +7,5 @@ import java.rmi.RemoteException;
  * Created by Benze on 8/23/15.
  */
 public interface MazePlayer extends Remote {
-
     public abstract String helloPlayer(String msg) throws RemoteException;
 }
