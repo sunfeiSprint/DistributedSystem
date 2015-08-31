ad0e4276a991214e856effb1188dbe368a51c03f
sunfeiXjoyin <afei01234@gmail.com>
2015-08-31 11:44:29 +0800

    mazeGame 2.0

--- mazeGameClientServer/src/mazeGameServerImpl.java
+++ mazeGameClientServer/src/mazeGameServerImpl.java
@@ -123,32 +123,7 @@ public class mazeGameServerImpl implements MazeGameServer,Runnable  {
 			System.exit(1);
 		}
 		
-//		System.out.println("Game Start");
-//		int numPlayers = 2;
-//		int mapSize = 10;
-//		int numTresures = 3;		
-//		GameStates game = new GameStates(mapSize, numPlayers,numTresures);
-//		
-//		Player playerZero = new Player("p0");
-//		
-//		playerZero.setLocationX(game.playerlocations.get("p0").getX());
-//		playerZero.setLocationY(game.playerlocations.get("p0").getY());
-//		
-//		System.out.println(game.toString());
-//		
-//		game = playerZero.move("S", game);
-//		
-//		System.out.println(game.toString());
-//		
-//		game = playerZero.move("E", game);
-//		System.out.println(game.toString());
-//		
-//		game = playerZero.move("W", game);
-//		
-//		System.out.println(game.toString());
-//		game = playerZero.move("N", game);
-//		
-//		System.out.println(game.toString());
+
 		
 	}
 }
